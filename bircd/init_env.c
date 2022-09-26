55b9fe87a38848d1edd7caa990f459d05d051ab4
hyudai28 <hayasakayudai@gmail.com>
2022-09-26 13:06:15 +0900

    add comment

--- bircd/init_env.c
+++ bircd/init_env.c
@@ -1,20 +1,24 @@
-
 #include <stdlib.h>
 #include <sys/resource.h>
 #include "bircd.h"
 
-void		init_env(t_env *e)
+//　env構造体に　ファイル上限数、maxfd、fdsを書き込む
+
+void		init_env(t_env *env)
 {
   int		i;
   struct rlimit	rlp;
 
+//　RLIMIT_NOFILE 開けるファイルの数 + 1　制限をかける
+//　x でやってること　＝　関数がエラー値だったらexitしてる
   X(-1, getrlimit(RLIMIT_NOFILE, &rlp), "getrlimit");
-  e->maxfd = rlp.rlim_cur;
-  e->fds = (t_fd*)Xv(NULL, malloc(sizeof(*e->fds) * e->maxfd), "malloc");
+//　TODO maxfdはどこで使うのか？何が７なのか
+  env->maxfd = rlp.rlim_cur;
+  env->fds = (t_fd*)Xv(NULL, malloc(sizeof(*env->fds) * env->maxfd), "malloc");
   i = 0;
-  while (i < e->maxfd)
+  while (i < env->maxfd)
     {
-      clean_fd(&e->fds[i]);
+      clean_fd(&env->fds[i]);
       i++;
     }
 }
--- bircd/srv_create.c
+++ bircd/srv_create.c
@@ -10,8 +10,12 @@ void			srv_create(t_env *e, int port)
   struct sockaddr_in	sin;
   struct protoent	*pe;
 
+ //getprotobyname("tcp"):データベースからtcpのエントリーを取ってきてる
+ // TODO エントリーってなんだよ
   pe = (struct protoent*)Xv(NULL, getprotobyname("tcp"), "getprotobyname");
+  //socket通信のエンドポイントを作成,通信方式としてはSOCK_STREAM
   s = X(-1, socket(PF_INET, SOCK_STREAM, pe->p_proto), "socket");
+  //アドレスファミリーによってソケット API で使用するアドレス構造の形式が決まる
   sin.sin_family = AF_INET;
   sin.sin_addr.s_addr = INADDR_ANY;
   sin.sin_port = htons(port);

e16eebd4f38ca47b6dbafb1e8a79acc7c3a03d8c
mfujishi <mfujishi@student.42tokyo.jp>
2022-09-28 21:06:32 +0900

    クライアントから送られてきた文字列を値と文字それぞれで確認できるようにしました。

--- bircd/client_read.c
+++ bircd/client_read.c
@@ -1,8 +1,8 @@
-
-#include <stdio.h>
 #include <unistd.h>
 #include <sys/socket.h>
 #include "bircd.h"
+
+# include <stdio.h>
 
 void	client_read(t_env *e, int cs)
 {
@@ -10,6 +10,28 @@ void	client_read(t_env *e, int cs)
   int	i;
 
   r = recv(cs, e->fds[cs].buf_read, BUF_SIZE, 0);
+  printf("%s", e->fds[cs].buf_read);
+  for (int i=0; e->fds[cs].buf_read[i] != '\0'; i++)
+  {
+    if (e->fds[cs].buf_read[i + 1] == '\0')
+    {
+      printf("[%3d]", e->fds[cs].buf_read[i]);
+      break ;
+    }
+    printf("[%3d]", e->fds[cs].buf_read[i]);
+  }
+  printf("\n");
+  for (int i=0; e->fds[cs].buf_read[i] != '\0'; i++)
+  {
+    if (e->fds[cs].buf_read[i] == 13)
+      printf("[ CR]");
+    else if (e->fds[cs].buf_read[i] == 10)
+      printf("[ LF]");
+    else
+      printf("[ %c ]", e->fds[cs].buf_read[i]);
+  }
+  printf("\n\n");
+
   if (r <= 0)
     {
       close(cs);

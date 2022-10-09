6445c4d5c9bf68f5a1ba7b58f000be26f84f8183
mfujishi <mfujishi@student.42tokyo.jp>
2022-10-09 21:27:31 +0900

    とりあえず接続と受信ができるようになったやつ

--- src/Server/Server.hpp
+++ src/Server/Server.hpp
@@ -1,18 +1,28 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
+# include "User.hpp"
+
+# include <vector>
+# include <map>
+# include <poll.h>
+
 class Server
 {
 	private:
 		int	port;
 		int	socket_fd;
+		std::vector<pollfd>	pollfds;
+		std::map<int, User *>	users;
 	public:
 		Server();
 		~Server();
 
-		void	server_start(int port);
+		void	start(int port);
+		void	loop();
 
 		int		get_port();
+		int		get_socket_fd();
 };
 
 #endif /* SERVER_HPP */
--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,52 @@
-#include <iostream>
-#include <stdlib.h>
-#include <netdb.h>
-#include <sys/socket.h>
-#include <sys/types.h>
+// #include <iostream>
+// #include <stdlib.h>
+// #include <netdb.h>
+// #include <sys/socket.h>
+// #include <sys/types.h>
 
 #include "Server.hpp"
 
-int	main(int argc, char *argv[])
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define SERVER_PORT  6667
+
+#define TRUE             1
+#define FALSE            0
+
+// int	main(int argc, char *argv[])
+// {
+	// int r;
+	// int fd;
+	// fd_set fd_read, fd_write;
+
+	// if (argc != 2)
+	// {
+	// 	std::cout << "./ft_irc port" << std::endl;
+	// }
+	// else
+	// {
+	// 	Server server;
+	// 	server.server_start(6667);
+	// 	server.loop();
+	// 	// while (1)
+	// 	// {
+	// 	// 	server.loop();
+	// 	// }
+	// }
+
+int	main (int argc, char *argv[])
 {
-	if (argc != 2)
-	{
-		std::cout << "./ft_irc port" << std::endl;
-	}
-	else
-	{
-		Server server;
-		server.server_start(6667);
-		while (1)
-		{
-			// poll();
-		}
-	}
+    Server server;
+
+    server.start(6667);
+	server.loop();
 }
